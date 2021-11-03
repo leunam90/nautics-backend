70e36727b486235812d65c95c2c3afae6983b4a0
Manuel Cervera <manuelcervera@MacBook-Pro-de-Manuel.local>
2021-11-03 10:59:36 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'http://sql11.freemysqlhosting.net/',
+    host: 'sql11.freemysqlhosting.net',
     user: 'sql11448759',
     password: 'fbQQaArk6I',
     database: 'sql11448759'
