05328f00f6937b7388df217004d8b279fe154649
Manuel Cervera <mcervera9001@gmail.com>
2021-11-07 12:47:38 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
-    host: 'sql11.freemysqlhosting.net',
-    user: 'sql11448759',
-    password: 'fbQQaArk6I',
-    database: 'sql11448759'
-});
-
 // const mysqlConnection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'test',
-//     password: 'test',
-//     database: 'project1'
+//     host: 'sql11.freemysqlhosting.net',
+//     user: 'sql11448759',
+//     password: 'fbQQaArk6I',
+//     database: 'sql11448759'
 // });
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    password: 'test',
+    database: 'project1'
+});
 
 mysqlConnection.connect((err) => {
     if (err) {
